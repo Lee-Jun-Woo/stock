cb57b9169bc86c3dc629e70bf5619197162df3cc
Jun Lee <ljw1029entry@naver.com>
2024-10-12 13:37:42 +0900

    price system changed

--- script.js
+++ script.js
@@ -25,6 +25,11 @@ function deleteStock(event) {
   saveStocks();
 }
 
+function makeRandom() {
+  const r = Math.random();
+  return r * 2 - 1;
+}
+
 function modifyStock(event) {
   const stock = event.target.parentElement;
   const id = parseInt(stock.id);
@@ -35,20 +40,15 @@ function modifyStock(event) {
     }
   }
 
-  const r = Math.random();
-  const curPrice = stocks[idx]["pri"];
-  let product = (-1.6 * (r ** 3 - r) + 0.6);
-  if (curPrice < 20) {
-    product += 0.4;
-  } else if (curPrice < 50) {
-    product += (-0.01 * curPrice + 0.5);
-  } else if (curPrice < 70) {
-  } else if (70 <= curPrice < 100) {
-    product += (-0.01 * curPrice + 0.7);
-  } else {
-    product -= 0.4;
-  }
-  stocks[idx]["pri"] *= product;
+  let flow = stocks[idx]["flo"];
+  flow += makeRandom() * 0.05;
+  flow = Math.tanh(12 * flow) / 20;
+  stocks[idx]["flo"] = flow;
+  let price = stocks[idx]["pri"];
+  price *= 1 + flow + makeRandom() * 0.03;
+  price = Math.tanh(0.025 * (price - 50)) / 0.025 + 50;
+  stocks[idx]["pri"] = price;
+
   saveStocks();
   location.reload();
 }
@@ -60,7 +60,9 @@ function paintStock(obj) {
   const deleteButton = document.createElement("button");
 
   stock.id = obj["id"];
-  stockContent.innerText = `${obj["nam"]}: ${Math.round(obj["pri"])}준우`;
+  stockContent.innerText = `${obj["nam"]}: ${
+    Math.round(obj["pri"] * 10) / 10
+  }준우`;
   modifyButton.innerText = "주가 갱신";
   deleteButton.innerText = "삭제";
   deleteButton.addEventListener("click", deleteStock);
@@ -83,7 +85,7 @@ function HandleCreate() {
   priceInput.value = "";
 
   if (name !== "" && 15 <= price && price <= 95) {
-    const obj = { nam: name, pri: price, id: Date.now() };
+    const obj = { nam: name, pri: price, flo: 0, id: Date.now() };
     stocks.push(obj);
     paintStock(obj);
     saveStocks();
